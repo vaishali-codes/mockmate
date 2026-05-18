65e521322b66aed3dbcf459f8dd3a67693032e00
Vaishali <singhvaishali699@gmail.com>
2026-05-18 16:38:35 +0530

    Add Vercel URL to CORS

--- main.py
+++ main.py
@@ -24,7 +24,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://prep-mate-ai-taupe.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
